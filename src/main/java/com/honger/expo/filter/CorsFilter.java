573aa699a53c80737e198baf988fb2e6d1945744
陈剑 <chenjian@chenjiandeMacBook-Pro.local>
2018-04-23 22:34:46 +0800

    ..

--- src/main/java/com/honger/expo/filter/CorsFilter.java
+++ src/main/java/com/honger/expo/filter/CorsFilter.java
@@ -28,10 +28,10 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "OPTIONS,POST,GET,DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "X-PINGOTHER,Content-Type,x-requested-with,Authorization");
         response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
---
+++ src/main/java/com/honger/expo/filter/WebConfig.java
@@ -0,0 +1,19 @@
+package com.honger.expo.filter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by chenjian on 2018/4/23.
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
