143b785d6c5d7813c048c481c52db2d6cf159354
陈剑 <chenjian@chenjiandeMacBook-Pro.local>
2018-04-25 18:13:47 +0800

    fix visa no file

--- src/test/java/com/honger/expo/dao/VisaDaoTest.java
+++ src/test/java/com/honger/expo/dao/VisaDaoTest.java
@@ -1,22 +1,16 @@
 package com.honger.expo.dao;
 
-import com.honger.expo.dto.response.home.CategoryListResponse;
 import com.honger.expo.dto.vo.VisaVO;
-import com.honger.expo.pojo.Category;
-import com.honger.expo.service.CategoryService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
 
+@WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})
 public class VisaDaoTest {
@@ -25,7 +19,7 @@ public class VisaDaoTest {
 
     @Test
     public void getVisaByCountryId(){
-        List<VisaVO> visaByCountryId = visaMapper.getVisaByCountryId("7");
+        List<VisaVO> visaByCountryId = visaMapper.getVisaByCountryId("89");
         System.out.println(visaByCountryId);
     }
 }
